780aa33761fd571c7350b62fd865dc07fb1d6aff
Rounak Choudhary <rounakrctechnical@gmail.com>
2024-03-18 22:07:02 +0530

    Unloop Changes

--- Assets/Unloop/Scripts/LineController.cs
+++ Assets/Unloop/Scripts/LineController.cs
@@ -56,6 +56,7 @@ public class LineController : MonoBehaviour
             lineRenderer.SetPosition(i, endPosition);
             yield return null;
         }
+        unloopPuzle.IsComplete = true;
         linePoints.Clear();
     }
 
